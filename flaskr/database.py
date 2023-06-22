0238e52a8f019eec956bbbcc4754ef8111408d21
Janier Almarza <janieralz@hotmail.com>
2023-06-22 07:50:32 -0400

    up

--- flaskr/database.py
+++ flaskr/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker, DeclarativeBase
 
 engine = create_engine('sqlite:///instance/test.db')
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+
+
+class Base(DeclarativeBase):
+    pass
 
 def init_db():
     Base.metadata.create_all(bind=engine)
